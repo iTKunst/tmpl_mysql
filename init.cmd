a35c46e54f64bd5982925e51e9f501f3245df092
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-27 14:22:56 -0400

    aa

--- init.cmd
+++ init.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-call LOG_ENTER \DIR_PROJ\init
+call LOG_ENTER \%DIR_PROJ%\init
 
-CALL  .\DIR_PROJ\env\init
+CALL  .\%DIR_PROJ%\env\init
 
-call LOG_EXIT \DIR_PROJ\init
+call LOG_EXIT \%DIR_PROJ%\init
