8c5c9c123df4d4b21a034861bf4ead5e282dc106
TAGHRdooleyp <pjldooley@gmail.com>
2021-01-25 01:42:09 -0500

    cc

--- env/init.cmd
+++ env/init.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 
-call LOG_ENTER \%DIR_PROJ%\env\init
+call LOG_ENTER \%DIR_TMPL%\env\init
 
-xcopy /q /y .\%DIR_PROJ%\env\win\*.cmd .\bin >NUL
+xcopy /q /y .\%DIR_TMPL%\env\win\*.cmd .\bin >NUL
 
-call LOG_EXIT \%DIR_PROJ%\env\init
+call LOG_EXIT \%DIR_TMPL%\env\init
